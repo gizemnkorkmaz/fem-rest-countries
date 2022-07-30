bf125db8289d4b8e7b26b2546346905862e6fe69
Gizem <gizemnurkorkmaz@gmail.com>
2022-07-30 18:13:17 +0300

    Add Icon

---
+++ src/components/Icon.vue
@@ -0,0 +1,27 @@
+<template>
+  <icomoon :iconSet="iconSet" v-bind="props" />
+</template>
+
+<script>
+import { Icomoon } from "vue-icomoon";
+import iconSet from "./selection.json";
+
+export default {
+  name: "Icon",
+  components: {
+    Icomoon,
+  },
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+  },
+  setup(props) {
+    return {
+      props,
+      iconSet,
+    };
+  },
+};
+</script>
