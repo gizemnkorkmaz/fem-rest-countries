a53d479ce1bba13c5dc0d87fb50fce19ddf29c7f
GizemKorkmaz <gizemkorkmaz@GizemKorkmazs-MacBook-Pro.local>
2022-07-30 21:17:38 +0300

    Add SearchBox

---
+++ src/components/SearchBox.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <Icon name="search" :size="12" class="relative left-10 text-slate-400" />
+    <input
+      type="text"
+      class="mx-4 my-12 py-3 pl-8 shadow-md rounded-sm text-sm w-72"
+      placeholder="Search for a country..."
+    />
+  </div>
+</template>
+
+<script setup>
+import Icon from "./Icon.vue";
+</script>
