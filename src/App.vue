3ca0e7752e84e805a09abc5df527ceff3f5fb1f1
GizemKorkmaz <gizemkorkmaz@GizemKorkmazs-MacBook-Pro.local>
2022-07-30 21:17:49 +0300

    Add search icon

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
 import HeaderComponent from "./components/Header.vue";
+import FlagBoard from "./components/FlagBoard.vue";
 </script>
 
 <template>
   <HeaderComponent />
+  <FlagBoard />
 </template>
 
 <style scoped></style>
