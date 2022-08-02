491f34dd34c3e447672fe9e94b7e5ddf40153f79
GizemKorkmaz <gizemkorkmaz@GizemKorkmazs-MacBook-Pro.local>
2022-08-02 19:59:16 +0300

    Add DropdownMenu

---
+++ src/components/DropdownMenu.vue
@@ -0,0 +1,10 @@
+<template>
+  <select class="border p-3 rounded text-slate-600">
+    <option value="" disabled selected>Filter by Region</option>
+    <option v-for="region in regions" :key="region">{{ region }}</option>
+  </select>
+</template>
+
+<script setup>
+const regions = ["Africa", "America", "Asia", "Europe", "Oceania", "Pacific"];
+</script>
