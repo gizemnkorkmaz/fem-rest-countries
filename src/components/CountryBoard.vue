0b9c93a7c09fa76c10fffd3fc78330d022a1b92a
GizemKorkmaz <gizemkorkmaz@GizemKorkmazs-MacBook-Pro.local>
2022-08-02 19:59:35 +0300

    Add Dropdown to CountryBoard

--- src/components/CountryBoard.vue
+++ src/components/CountryBoard.vue
@@ -1,7 +1,14 @@
+<template>
+  <section class="query-section flex items-center justify-between px-10 mx-5">
+    <SearchBox />
+    <DropdownMenu />
+  </section>
+
+  <CountryBox />
+</template>
+
 <script setup>
 import SearchBox from "./SearchBox.vue";
+import CountryBox from "./CountryBox.vue";
+import DropdownMenu from "./DropdownMenu.vue";
 </script>
-
-<template>
-  <SearchBox />
-</template>
