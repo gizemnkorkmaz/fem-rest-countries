43cb2a95544203db7bc48e103bf4c977d646fa8e
Gizem <gizemnurkorkmaz@gmail.com>
2022-08-29 22:09:22 +0300

    Fix theme switcher

--- src/components/ThemeSwitcher.vue
+++ src/components/ThemeSwitcher.vue
@@ -3,14 +3,13 @@
     class="flex items-center font-semibold dark:text-white cursor-pointer"
     @click="switchTheme"
   >
-    <icon :name="theme" :size="18" />
-    <span class="px-2 text-md"> {{ _.capitalize(theme) }} Mode</span>
+    <icon name="dark" :size="18" v-if="theme == 'light'" />
+    <icon name="light" :size="18" v-if="theme == 'dark'" />
   </div>
 </template>
 
 <script setup>
 import { inject } from "@vue/runtime-core";
-import _ from "lodash";
 
 import Icon from "./Icon.vue";
 
